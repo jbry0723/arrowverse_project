33c771e0156bbc1bbb9962e81ad8ddb1c5838359
Justin Bryan <jbry0723@gmail.com>
2022-02-14 03:25:57 -0600

    fixed loading screen color

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
         setData({
           showData: showResp,
           castData: castResp,
-          isLoading: false,
+          isLoading: true,
           csvFile: csv,
         });
       }
